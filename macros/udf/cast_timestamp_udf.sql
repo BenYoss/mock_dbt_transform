7e5a791203b23b1bcfd46dfaa12ea7a8a321dd26
benyoss <benyoss4@gmail.com>
2025-07-14 16:45:40 -0500

    (fix) UDF syntax error.

--- macros/udf/cast_timestamp_udf.sql
+++ macros/udf/cast_timestamp_udf.sql
@@ -1,9 +1,2 @@
 {% macro create_cast_timestamp_udf() %}
-    {% if target.type == 'duckdb' %}
-        {{ return('') }}
-    {% endif %}
-    {{ config(materialized='statement') }}
-    CREATE FUNCTION cast_timestamp_udf(input VARCHAR) 
-    RETURNS TIMESTAMP AS CAST(input AS TIMESTAMP)
-    LANGUAGE sql AS $$;
 {% endmacro %}
